8da3914b8aa9f52bde2992fe22dd662060364f9c
IlyaOsipenkov <dakarlog@gmail.com>
2022-09-27 23:19:54 +0300

    last swiper

--- src/js/main.js
+++ src/js/main.js
@@ -15,17 +15,21 @@ const slidesPerView = () => {
     return (screenWidth) / (slide + 8)
 }
 
+const slidesPerViewScnd = () => {
+    const slide = document.getElementsByClassName('swiper-gadgets__item')[0].clientWidth
+    return (screenWidth) / (slide + 8)
+}
 
 
 if (isMobile(screenWidth)) {
-    swiper = new Swiper('.swiper', {
+    swiper = new Swiper('.swiper-repair', {
         pagination: {
-            el: '.swiper-pagination',
+            el: '.swiper-pagination-repair',
             type: 'bullets',
             clickable: true,
         },
         slidesPerView: slidesPerView(),
-        spaceBetween: 16,
+        spaceBetween: 32,
         updateOnWindowResize: true,
     })
 }
@@ -49,14 +53,14 @@ const resize = () => {
     console.log(screenWidth)
     if (swiper != undefined) {
         if (isMobile(screenWidth)) {
-            swiper = new Swiper('.swiper', {
+            swiper = new Swiper('.swiper-repair', {
                 pagination: {
-                    el: '.swiper-pagination',
+                    el: '.swiper-pagination-repair',
                     type: 'bullets',
                     clickable: true,
                 },
                 slidesPerView: slidesPerView(),
-                spaceBetween: 16,
+                spaceBetween: 32,
                 updateOnWindowResize: true,
             })
         } else {
@@ -71,7 +75,7 @@ window.addEventListener('resize', resize)
 
 
 const btnRepairBrandsMoreInfo = document.querySelector('.repair-brands__btn-more-info')
-const swiperClass = document.querySelector('.swiper')
+const swiperClass = document.querySelector('.swiper-repair')
 // const btnDivMoreInfo = document.getElementsByClassName('repair-brands__wrapper-more-info')
 
 let buttonState = false
@@ -84,11 +88,11 @@ const clickHandler = () => {
         if (!state) {
             text.innerHTML = 'Скрыть'
             arrows.style.transform = 'rotate(180deg)'
-            swiperClass.style.height = '270px'
+            swiperClass.style.height = '296px'
         } else {
             text.innerHTML = 'Показать все'
             arrows.style.transform = 'rotate(0)'
-            swiperClass.style.height = '180px'
+            swiperClass.style.height = '192px'
         }
         buttonState = !buttonState
     }
@@ -97,3 +101,73 @@ const clickHandler = () => {
 
 btnRepairBrandsMoreInfo.addEventListener('click', clickHandler)
 
+
+
+//2nd swiper options
+
+if (isMobile(screenWidth)) {
+    swiper = new Swiper('.swiper-gadgets', {
+        pagination: {
+            el: '.swiper-pagination-gadgets',
+            type: 'bullets',
+            clickable: true,
+        },
+        slidesPerView: slidesPerViewScnd(),
+        spaceBetween: 32,
+        updateOnWindowResize: true,
+    })
+}
+
+const resizeGadgets = () => {
+    screenWidth = window.innerWidth || document.documentElement.clientWidth ||
+        document.body.clientWidth
+    console.log(screenWidth)
+    if (swiper != undefined) {
+        if (isMobile(screenWidth)) {
+            swiper = new Swiper('.swiper-gadgets', {
+                pagination: {
+                    el: '.swiper-pagination-gadgets',
+                    type: 'bullets',
+                    clickable: true,
+                },
+                slidesPerView: slidesPerViewScnd(),
+                spaceBetween: 32,
+                updateOnWindowResize: true,
+            })
+        } else {
+            swiper.destroy(false, false)
+            swiper.disable()
+        }
+    }
+}
+
+
+window.addEventListener('resize', resizeGadgets)
+
+
+const btnMoreInfoGadgets = document.querySelector('.gadgets__btn-more-info')
+const swiperGadgets = document.querySelector('.swiper-gadgets')
+// const btnDivMoreInfo = document.getElementsByClassName('repair-brands__wrapper-more-info')
+
+let buttonStateGadgets = false
+
+
+const clickHandlerGadgets = () => {
+    const [arrows, text] = btnMoreInfoGadgets.children
+    console.log('click handler')
+    const toggle = (state) => {
+        if (!state) {
+            text.innerHTML = 'Скрыть'
+            arrows.style.transform = 'rotate(180deg)'
+            swiperGadgets.style.height = '560px'
+        } else {
+            text.innerHTML = 'Показать все'
+            arrows.style.transform = 'rotate(0)'
+            swiperGadgets.style.height = '176px'
+        }
+        buttonStateGadgets = !buttonStateGadgets
+    }
+    toggle(buttonStateGadgets)
+}
+
+btnMoreInfoGadgets.addEventListener('click', clickHandlerGadgets)
